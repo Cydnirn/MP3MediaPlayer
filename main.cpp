24e756c7c238d07998031604e0d9395ba2bc5bbd
Seno Aji <cydnirn.dev@gmail.com>
2025-06-29 22:45:14 +0700

    refactor: simplify main loop and add exit option in main function

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 #include <memory>
 #include "PlayMP3.h"
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
+int main()
+{
     std::string x,y;
     int pos;
     int menu;
+    bool start = true;
 
-    while(true) {
-
+    while(start) {
         nowPlaying();
         std::cout << std::endl;
         std::cout << "Pilih Menu dibawah : \n";
@@ -32,19 +32,16 @@ int main() {
             std::cin >> y;
             addMusic(x,y);
             break;
-
         case 2:
             std::cout << "Nomer urutan musik yang ingin dihapus : ";
             std::cin >> pos;
             removeMusic(pos);
             break;
+        case 6:
+            start = false;
+        default:
+            break;
         }
-
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
     }
-
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
